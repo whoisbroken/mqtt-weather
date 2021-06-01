49279b55c87d5addea6b9500e59060158c8c06a7
Oleksandr Petryha <oleksandr.petryha@kevurugames.com>
2021-06-01 16:45:00 +0300

    added comments

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// Подключение библиотек
 const fetch = require('node-fetch');
 const config = require('./config.json');
 const mqtt = require('mqtt');
 const api = require('./weatherapi.json');
 
+// Присоединяемся по mqtt протоколу к брокеру ably и сохраняем её в переменную 
 const client = mqtt.connect('mqtts://mqtt.ably.io');
 
+// При коннекте подписываемся на токен погоды
 client.on('connect', (options) => {
   client.subscribe(`${config.token}/weather`, (err) => {
     if (err) {
@@ -15,11 +18,13 @@ client.on('connect', (options) => {
   console.log('Connected successfully');
 });
 
+// Формируем сообщение
 client.on('message', async (city) => {
   const weather = await getWeather(city);
   console.log(weather);
 });
 
+// Асинхронная функция которая делает запрос на api погоды
 async function getWeather(city) {
   const url = `${api.url}?q=${city || api.city}&appid=${api.apiKey}&units=${
     api.units
@@ -27,6 +32,7 @@ async function getWeather(city) {
 
   const response = await (await fetch(url)).json();
   return response;
-}
+};
 
-client.emit('message', 'Dnipro')
+// Эмитируем сообщение с городом Днипро
+client.emit('message', 'Dnipro');
